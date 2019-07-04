6bbd5d45a20a619e4c08427c705907c9e03daf80
rama99 <rama.kishore@gmail.com>
2019-07-04 14:48:05 +0530

    few changes

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://ravi:rajanipriya@advancednode-shard-00-00-st7q7.mongodb.net:27017,advancednode-shard-00-01-st7q7.mongodb.net:27017,advancednode-shard-00-02-st7q7.mongodb.net:27017/test?ssl=true&replicaSet=advancedNode-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongoURI: 'mongodb://ravi:test@advancednode-shard-00-00-st7q7.mongodb.net:27017,advancednode-shard-00-01-st7q7.mongodb.net:27017,advancednode-shard-00-02-st7q7.mongodb.net:27017/test?ssl=true&replicaSet=advancedNode-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
